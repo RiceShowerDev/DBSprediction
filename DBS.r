10ae0cd01249db8476309a33c911a1211e6e9164
Zou <102339870+RiceShowerDev@users.noreply.github.com>
2022-07-07 09:12:47 +0800

    Delete DBS.r

--- DBS.r
+++
@@ -1,8 +0,0 @@
-d=read.csv("D:/PROGRAMS/NTU/DBS_SingDollar.csv")
-model = lm(d$DBS ~ d$SGD,data=d)
-pred=predict(model,newdata=d)
-print(pred)
-err=d$DBS - pred
-print(err)
-rmse=mean(err^2)^0.5
-print(rmse)
